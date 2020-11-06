370b1df8b5a3217d0da9d879f567cf343ada8724
Diego Cordova <diego.cordova.do.prado@gmail.com>
2020-11-05 20:27:20 -0400

    DashboardRequest dto crated

---
+++ src/main/java/bo/ucb/edu/ingsoft/dto/DashboardRequest.java
@@ -0,0 +1,93 @@
+package bo.ucb.edu.ingsoft.dto;
+
+public class DashboardRequest {
+    private String publisher;
+    private String email;
+    private String paypal;
+    private Integer sells;
+    private Double earnings;
+    private Double monthly_earnings;
+    private String country;
+    private String game;
+
+    public DashboardRequest() {
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPaypal() {
+        return paypal;
+    }
+
+    public void setPaypal(String paypal) {
+        this.paypal = paypal;
+    }
+
+    public Integer getSells() {
+        return sells;
+    }
+
+    public void setSells(Integer sells) {
+        this.sells = sells;
+    }
+
+    public Double getEarnings() {
+        return earnings;
+    }
+
+    public void setEarnings(Double earnings) {
+        this.earnings = earnings;
+    }
+
+    public Double getMonthly_earnings() {
+        return monthly_earnings;
+    }
+
+    public void setMonthly_earnings(Double monthly_earnings) {
+        this.monthly_earnings = monthly_earnings;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getGame() {
+        return game;
+    }
+
+    public void setGame(String game) {
+        this.game = game;
+    }
+
+    @Override
+    public String toString() {
+        return "DashboardRequest{" +
+                "publisher='" + publisher + '\'' +
+                ", email='" + email + '\'' +
+                ", paypal='" + paypal + '\'' +
+                ", sells=" + sells +
+                ", earnings=" + earnings +
+                ", monthly_earnings=" + monthly_earnings +
+                ", country='" + country + '\'' +
+                ", game='" + game + '\'' +
+                '}';
+    }
+}
