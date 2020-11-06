9a50c1cebfd72ab005e5168799015d08f5b5e000
Sergio <sergioivanprudencio@gmail.com>
2020-11-06 08:23:34 -0400

    create developer model

---
+++ src/main/java/bo/ucb/edu/ingsoft/models/Developers.java
@@ -0,0 +1,46 @@
+package bo.ucb.edu.ingsoft.models;
+
+public class Developers {
+    private Integer idDeveloper;
+    private Integer idPublisher;
+    private String developer;
+
+    public Developers(Integer idDeveloper, Integer idPublisher, String developer) {
+        this.idDeveloper = idDeveloper;
+        this.idPublisher = idPublisher;
+        this.developer = developer;
+    }
+
+    @Override
+    public String toString() {
+        return "Developers{" +
+                "idDeveloper=" + idDeveloper +
+                ", idPublisher=" + idPublisher +
+                ", developer='" + developer + '\'' +
+                '}';
+    }
+
+    public Integer getIdDeveloper() {
+        return idDeveloper;
+    }
+
+    public void setIdDeveloper(Integer idDeveloper) {
+        this.idDeveloper = idDeveloper;
+    }
+
+    public Integer getIdPublisher() {
+        return idPublisher;
+    }
+
+    public void setIdPublisher(Integer idPublisher) {
+        this.idPublisher = idPublisher;
+    }
+
+    public String getDeveloper() {
+        return developer;
+    }
+
+    public void setDeveloper(String developer) {
+        this.developer = developer;
+    }
+}
