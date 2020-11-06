b4a5bf852cfd85e047be0c0ab4e0c5d7113abd32
Martín Campuzano Flores <martin.campuzano.flores@gmail.com>
2020-11-05 21:50:28 -0400

    Latest Releases DTO added

---
+++ src/main/java/bo/ucb/edu/ingsoft/dto/ReleaseRequest.java
@@ -0,0 +1,12 @@
+package bo.ucb.edu.ingsoft.dto;
+
+import java.sql.Date;
+
+public class ReleaseRequest {
+    private String id;
+    private String title;
+    private String game_description;
+    private Double price;
+    private String banner;
+    private Date release_date;
+}
