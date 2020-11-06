6522ea4c7ff1ee65738106e1b763f2ed0dc1306b
JuancaJcA <avilafloresjc@gmail.com>
2020-11-05 20:12:51 -0400

    UserRequest DTO created

---
+++ src/main/java/bo/ucb/edu/ingsoft/dto/UserRequest.java
@@ -0,0 +1,74 @@
+package bo.ucb.edu.ingsoft.dto;
+
+public class UserRequest {
+    private String username;
+    private String name;
+    private String lastname;
+    private String alias;
+    private String email;
+    private String country;
+
+    public UserRequest(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "UserRequest{" +
+                "username='" + username + '\'' +
+                ", name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", alias='" + alias + '\'' +
+                ", email='" + email + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
