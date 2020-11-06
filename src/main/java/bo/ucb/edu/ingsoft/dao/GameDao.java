9906ccc4580b8ac4fe3f2167e8f94b6c8d48fd48
Martín Campuzano Flores <martin.campuzano.flores@gmail.com>
2020-11-06 18:12:42 -0400

    Added Highlights in gamedao

--- src/main/java/bo/ucb/edu/ingsoft/dao/GameDao.java
+++ src/main/java/bo/ucb/edu/ingsoft/dao/GameDao.java
@@ -6,10 +6,12 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Mapper
 public interface GameDao {
     // Get Sale games data
     public ArrayList<Game> findById(ArrayList<Integer> idGame);
-    public ArrayList<Game> getPage(@Param("a") Integer limit, @Param("b") Integer offset);
+    public ArrayList<Game> findPage(@Param("a") Integer limit, @Param("b") Integer offset);
+    public List<Game> getfindHighlight();
 }
