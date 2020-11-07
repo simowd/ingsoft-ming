626d2a338eb606237129c926d6624081a946453a
Martín Campuzano Flores <martin.campuzano.flores@gmail.com>
2020-11-07 11:08:05 -0400

    Added HomePage GET

--- src/main/java/bo/ucb/edu/ingsoft/api/StoreApi.java
+++ src/main/java/bo/ucb/edu/ingsoft/api/StoreApi.java
@@ -1,8 +1,13 @@
 package bo.ucb.edu.ingsoft.api;
 
 import bo.ucb.edu.ingsoft.bl.StoreBl;
+import bo.ucb.edu.ingsoft.dto.HomepageRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
 
 @RestController
 public class StoreApi {
@@ -12,6 +17,9 @@ public class StoreApi {
     public StoreApi(StoreBl storeBl) {
         this.storeBl = storeBl;
     }
-
+    @RequestMapping(value="/ming/store/homepage", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getHomepage(@RequestParam Integer page){
+        return "ID" + page;
+    }
 
 }
