d191b0877f5b466f2e239618b2545209ead437b8
Fulano2141 <macuto2142dt@gmail.com>
2020-11-07 19:19:09 -0400

    Publisher inherits from Transaction

--- src/main/java/bo/ucb/edu/ingsoft/models/Publisher.java
+++ src/main/java/bo/ucb/edu/ingsoft/models/Publisher.java
@@ -3,34 +3,33 @@ package bo.ucb.edu.ingsoft.models;
 
 import java.util.Date;
 
-public class Publisher {
+public class Publisher extends Transactions {
     private Integer idPublisher;
     private Integer idUser;
     private String publisher;
     private String paypalMail;
     private Integer status;
-    private Integer txId;
-    private String txHost;
-    private Integer txUserId;
-    private Date txDate;
+
 
     public Publisher() {
 
     }
 
-    @Override
-    public String toString() {
-        return "Publisher{" +
-                "idPublisher=" + idPublisher +
-                ", idUser=" + idUser +
-                ", publisher='" + publisher + '\'' +
-                ", paypalMail='" + paypalMail + '\'' +
-                ", status=" + status +
-                ", txId=" + txId +
-                ", txHost='" + txHost + '\'' +
-                ", txUserId=" + txUserId +
-                ", txDate=" + txDate +
-                '}';
+    public Publisher(Integer idPublisher, Integer idUser, String publisher, String paypalMail, Integer status) {
+        this.idPublisher = idPublisher;
+        this.idUser = idUser;
+        this.publisher = publisher;
+        this.paypalMail = paypalMail;
+        this.status = status;
+    }
+
+    public Publisher(Integer txId, String txHost, Integer txUser, Date txDate, Integer idPublisher, Integer idUser, String publisher, String paypalMail, Integer status) {
+        super(txId, txHost, txUser, txDate);
+        this.idPublisher = idPublisher;
+        this.idUser = idUser;
+        this.publisher = publisher;
+        this.paypalMail = paypalMail;
+        this.status = status;
     }
 
     public Integer getIdPublisher() {
@@ -73,35 +72,14 @@ public class Publisher {
         this.status = status;
     }
 
-    public Integer getTxId() {
-        return txId;
-    }
-
-    public void setTxId(Integer txId) {
-        this.txId = txId;
-    }
-
-    public String getTxHost() {
-        return txHost;
-    }
-
-    public void setTxHost(String txHost) {
-        this.txHost = txHost;
-    }
-
-    public Integer getTxUserId() {
-        return txUserId;
-    }
-
-    public void setTxUserId(Integer txUserId) {
-        this.txUserId = txUserId;
-    }
-
-    public Date getTxDate() {
-        return txDate;
-    }
-
-    public void setTxDate(Date txDate) {
-        this.txDate = txDate;
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "idPublisher=" + idPublisher +
+                ", idUser=" + idUser +
+                ", publisher='" + publisher + '\'' +
+                ", paypalMail='" + paypalMail + '\'' +
+                ", status=" + status +
+                '}';
     }
 }
