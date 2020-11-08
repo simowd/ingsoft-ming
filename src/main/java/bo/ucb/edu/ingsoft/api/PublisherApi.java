ec1511cecea2b4b9d63f55095a65f1ee8400b8f0
Diego Cordova <diego.cordova.do.prado@gmail.com>
2020-11-07 20:47:32 -0400

    Changes made to Publisher.http

--- src/main/java/bo/ucb/edu/ingsoft/api/PublisherApi.java
+++ src/main/java/bo/ucb/edu/ingsoft/api/PublisherApi.java
@@ -89,9 +89,9 @@ public class PublisherApi {
     }
 
     @RequestMapping(value="/ming/publisher/{id}/dashboard", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public DashboardRequest PublisherDashboard(@RequestParam Integer idPublisher){
+    public DashboardRequest PublisherDashboard(@PathVariable("id") Integer idPublisher){
         try {
-            return publisherBl.PublisherDashboard(1);
+            return publisherBl.PublisherDashboard(idPublisher);
         } catch (Exception ex) {
             throw new ResponseStatusException(
                     HttpStatus.NOT_FOUND, "Page Not Found", ex);
