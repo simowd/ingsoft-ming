70d26a488a651887762554c06a071eecbef02816
Sergio <sergioivanprudencio@gmail.com>
2020-11-08 14:59:48 -0400

    admi http

--- src/main/java/bo/ucb/edu/ingsoft/api/PublisherApi.java
+++ src/main/java/bo/ucb/edu/ingsoft/api/PublisherApi.java
@@ -32,15 +32,7 @@ public class PublisherApi {
     }
 
 
-    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE,
-            consumes = MediaType.APPLICATION_JSON_VALUE,value = "/ming/admin/publisher")
 
-    public PublisherRequest createPublisher(@RequestBody PublisherRequest publisherRequest, HttpServletRequest request){
-        Transaction transaction = TransactionUtil.createTransaction(request);
-        transactionBl.createTransaction(transaction);
-        PublisherRequest publisherResponse = publisherBl.createPublisher(publisherRequest, transaction);
-        return publisherResponse;
-    }
 
     @RequestMapping(value = "/ming/publisher/{id}",method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public PublisherRequest findById (@PathVariable("id") Integer userId) {
@@ -52,28 +44,7 @@ public class PublisherApi {
         }
     }
 
-    @RequestMapping(value = "/ming/admin/publisher",method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<PublisherRequest> publisherList() {
-        try {
-            return publisherBl.getPublisherList();
-        } catch (Exception ex) {
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND, "User Not Found", ex);
-        }
-    }
 
-    @RequestMapping(value = "/ming/admin/publisher/{id}",method = RequestMethod.DELETE)
-    public @ResponseBody String deletePublisher(@PathVariable("id") Integer userId,  HttpServletRequest request) {
-        try {
-            Transaction transaction = TransactionUtil.createTransaction(request);
-            transactionBl.createTransaction(transaction);
-            publisherBl.deletePublisher(userId,transaction);
-            return "ok";
-        } catch (Exception ex) {
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND, "User Not Found", ex);
-        }
-    }
 
     @RequestMapping(value = "/ming/publisher/{id}",method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody String updatePublisher(@PathVariable("id") Integer userId,@RequestBody PublisherRequest publisherRequest, HttpServletRequest request) {
